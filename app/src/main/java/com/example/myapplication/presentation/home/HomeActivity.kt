32065d02a2d246afa5284dd9713dc68830c9200a
dasol kim <sorikiki98@naver.com>
2021-09-28 01:48:00 +0900

    [ADD] home navigation drawer layout

--- app/src/main/java/com/example/myapplication/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/presentation/home/HomeActivity.kt
@@ -2,13 +2,18 @@ package com.example.myapplication.presentation.home
 
 import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.marginTop
+import androidx.core.view.size
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.R
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity: AppCompatActivity() {
 
@@ -23,7 +28,19 @@ class HomeActivity: AppCompatActivity() {
         val navigationController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment).navController
         val toolBar = findViewById<Toolbar>(R.id.tool_bar)
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        var navigationDrawer = findViewById<NavigationView>(R.id.nav_view)
 
         toolBar.setupWithNavController(navigationController, drawerLayout)
+
+        navigationDrawer.apply {
+            layoutParams.width = Resources.getSystem().displayMetrics.widthPixels * 3 / 4
+            inflateHeaderView(R.layout.main_menu_header)
+        }
+
+
+        navigationDrawer.getHeaderView(0).apply {
+            layoutParams.width = navigationDrawer.layoutParams.width
+        }
+
     }
 }
