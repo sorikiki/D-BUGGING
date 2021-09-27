939de4ce55de75266f42b82d4eae94f35ed18b7b
sooyeon <tndus77@sookmyung.ac.kr>
2021-09-27 13:39:00 +0900

    merge

--- app/src/main/java/com/example/myapplication/presentation/register/RegisterContract.kt
+++ app/src/main/java/com/example/myapplication/presentation/register/RegisterContract.kt
@@ -1,15 +1,21 @@
 package com.example.myapplication.presentation.register
 
+import android.widget.EditText
+import com.example.myapplication.data.api.UserInfo
 import com.example.myapplication.presentation.BasePresenter
 import com.example.myapplication.presentation.BaseView
 
 interface RegisterContract {
     interface View : BaseView<Presenter> {
+        fun putUserInput()
 
+        fun processRegisterSuccess()
+
+        fun processRegisterFail()
 
     }
 
     interface Presenter : BasePresenter {
-
+        fun signUpUser(UserInfo: UserInfo);
     }
 }
--- app/src/main/java/com/example/myapplication/presentation/register/RegisterPresenter.kt
+++ app/src/main/java/com/example/myapplication/presentation/register/RegisterPresenter.kt
@@ -1,10 +1,33 @@
 package com.example.myapplication.presentation.register
 
+import com.example.myapplication.data.api.UserInfo
 import com.example.myapplication.data.repository.UserRepository
+import com.example.myapplication.di.appModule
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 class RegisterPresenter(
     private val userRepository: UserRepository,
-    private val view:RegisterFragment
-    ) {
+    private val view:RegisterContract.View
+):RegisterContract.Presenter {
+
+    override val scope: CoroutineScope = MainScope()
+
+    override fun onViewCreated() {}
+
+    override fun onDestroyView() {}
+
+    override fun signUpUser(UserInfo: UserInfo) {
+        scope.launch {
+            val isSucceed = userRepository.registerUser(UserInfo)
+
+            if (isSucceed) {
+                view.processRegisterSuccess()
+            } else {
+                view.processRegisterFail()
+            }
+        }
+    }
 
 }
