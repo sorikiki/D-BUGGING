cd1aa7e95d55f73795f78c7884178317a0bd30be
Jiseondf <wpdldptm109@naver.com>
2021-09-27 22:31:45 +0900

    company, product list

--- app/src/main/java/com/example/myapplication/presentation/company/CompanyListFragment.kt
+++ app/src/main/java/com/example/myapplication/presentation/company/CompanyListFragment.kt
@@ -1,25 +1,52 @@
 package com.example.myapplication.presentation.company
 
+import CompanyListAdapter
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentCompanyListBinding
+import java.util.ArrayList
 
 class CompanyListFragment: Fragment() {
+    lateinit var companyRecyclerView: RecyclerView
+    lateinit var companyAdapter: CompanyListAdapter
+    var companyItems: ArrayList<CompanyListItem> = ArrayList<CompanyListItem>()
+
     private var binding: FragmentCompanyListBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View = FragmentCompanyListBinding.inflate(inflater, container, false)
-        .also { binding = it }
+        .also {
+            binding = it
+            companyRecyclerView= binding!!.rvCompanyList
+            //리사이클러뷰에 adapter 객체 지정
+            companyAdapter = CompanyListAdapter(companyItems)
+            companyRecyclerView.setAdapter(companyAdapter)
+            var layoutManager = LinearLayoutManager(context) // 리사이클러뷰 그리드 레이아웃
+            companyRecyclerView.setLayoutManager(layoutManager)
+        }
         .root
 
+    fun addItem(name:String, grade: Double, intro:String) {
+        val item = CompanyListItem(name, grade, intro)
+        item.setName(name)
+        item.setGrade(grade)
+        item.setIntro(intro)
+        companyItems.add(item)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/myapplication/presentation/product/ProductListAdapter.kt
@@ -0,0 +1,90 @@
+package com.example.myapplication.presentation.product
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+import java.util.ArrayList
+
+class ProductListAdapter internal constructor(list: ArrayList<ProductListItem>?) :
+    RecyclerView.Adapter<ProductListAdapter.ViewHolder>() {
+    private var mData: ArrayList<ProductListItem>? = null
+
+    // 클릭 이벤트 리스너 인터페이스 정의
+    interface OnItemClickListener {
+        fun onItemClick(v: View?, pos: Int)
+    }
+
+    private var mListener: OnItemClickListener? = null //전달된 리스너 객체를 저장할 변수
+
+    //리스터 객체를 전달하는 메소드
+    fun setOnItemClickListener(listener: OnItemClickListener?) {
+        mListener = listener
+    }
+
+    // onCreateViewHolder() - 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater =
+            context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = inflater.inflate(R.layout.item_product_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시.
+    @SuppressLint("UseCompatLoadingForDrawables")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = mData!![position]
+
+        //썸네일
+        //val drawable =
+        //holder.itemView.context.getDrawable(R.drawable.ic_fo_search_empty)
+        //holder.thumb.background = drawable
+        //  이름, 인트로
+        holder.name.setText(item.getName())
+        holder.intro.setText(item.getIntro())
+
+    }
+
+    // getItemCount() - 전체 데이터 갯수 리턴.
+    override fun getItemCount(): Int {
+        return mData!!.size
+    }
+
+    // 아이템 뷰를 저장하는 뷰홀더 클래스.
+    inner class ViewHolder internal constructor(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        var thumb: ImageView
+        var name: TextView
+        var intro: TextView
+
+        init {
+            // 뷰 객체에 대한 참조. (hold strong reference)
+            thumb = itemView.findViewById(R.id.iv_product_thumb)
+            name = itemView.findViewById(R.id.tv_product_title)
+            intro = itemView.findViewById(R.id.tv_product_intro)
+
+            //아이템 클릭이벤트처리
+            itemView.setOnClickListener { v ->
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    if (mListener != null) {
+                        mListener!!.onItemClick(v, position)
+                    }
+                }
+            }
+
+        }
+    }
+
+    // 생성자에서 데이터 리스트 객체를 전달받음.
+    init {
+        mData = list
+    }
+
+}
