3a090729a6576a015c36f426b431669db0fd3c7a
dasol kim <sorikiki98@naver.com>
2021-09-27 21:55:37 +0900

    [ADD] home main xml

---
+++ app/src/main/java/com/example/myapplication/presentation/home/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.presentation.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentHomeBinding
+
+class HomeFragment: Fragment() {
+    private var binding: FragmentHomeBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentHomeBinding.inflate(inflater, container, false)
+        .also { binding = it }
+        .root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
