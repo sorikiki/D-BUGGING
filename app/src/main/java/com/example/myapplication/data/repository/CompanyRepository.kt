9e639b306b542f844f11416711691f3dde5c64f7
sooyeon <tndus77@sookmyung.ac.kr>
2021-11-02 01:54:41 +0900

    [ADD] reserveCompany

--- app/src/main/java/com/example/myapplication/data/repository/CompanyRepository.kt
+++ app/src/main/java/com/example/myapplication/data/repository/CompanyRepository.kt
@@ -1,9 +1,15 @@
 package com.example.myapplication.data.repository
 
+import com.example.myapplication.data.api.CompanyApi
+import com.example.myapplication.data.api.ReservationInfo
 import com.example.myapplication.domain.CompanyInformation
 
 interface CompanyRepository {
     suspend fun getCompanyList(): List<CompanyInformation>?
 
     suspend fun updateCompanyFavorite(companyId: Int, isFavorite: Boolean)
+
+    suspend fun makeReservation(companyId: Int, reservationInfo: ReservationInfo)
+
+    suspend fun requestInformation(companyId: Int, reservationId: Int)
 }
--- app/src/main/java/com/example/myapplication/data/repository/CompanyRepositoryImpl.kt
+++ app/src/main/java/com/example/myapplication/data/repository/CompanyRepositoryImpl.kt
@@ -2,9 +2,15 @@ package com.example.myapplication.data.repository
 
 import android.util.Log
 import com.example.myapplication.data.api.CompanyApi
+import com.example.myapplication.data.api.ReservationInfo
+import com.example.myapplication.data.api.response.ReservationCheckResponse
+import com.example.myapplication.data.api.response.ReservationResponse
 import com.example.myapplication.data.api.response.mapper.toCompanyInformation
 import com.example.myapplication.data.preference.PreferenceManager
 import com.example.myapplication.domain.CompanyInformation
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 class CompanyRepositoryImpl(
     private val companyApi: CompanyApi,
@@ -32,5 +38,44 @@ class CompanyRepositoryImpl(
         } else {
             companyApi.removeCompanyItemFromWishList(userId!!, companyId)
         }
+    }
+
+    override suspend fun makeReservation(companyId: Int, reservationInfo: ReservationInfo) {
+        val call = companyApi.reserveCompany(companyId, reservationInfo)
+
+        call.enqueue(object : retrofit2.Callback<ReservationResponse> {
+            override fun onFailure(call: Call<ReservationResponse>, t: Throwable) {
+                Log.d("response", t.toString())
+            }
+
+            override fun onResponse(
+                call: Call<ReservationResponse>,
+                response: Response<ReservationResponse>
+            ) {
+                if(response.isSuccessful) {
+                    Log.d("response", response?.body().toString())
+                    //response.body()?.reservation?.reservationId
+                }
+            }
+        })
+    }
+
+    override suspend fun requestInformation(companyId: Int, reservationId : Int) {
+        val callReserveCompany = companyApi.checkCompanyReservation(companyId, reservationId)
+
+        callReserveCompany.enqueue(object : retrofit2.Callback<ReservationCheckResponse> {
+            override fun onResponse(
+                call: Call<ReservationCheckResponse>,
+                response: Response<ReservationCheckResponse>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d("response", response?.body().toString())
+                }
+            }
+
+            override fun onFailure(call: Call<ReservationCheckResponse>, t: Throwable) {
+                Log.d("response", t.toString())
+            }
+        })
     }
 }
