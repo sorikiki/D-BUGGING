33aeb0133b0fe9cc62dd58564c96c0c62b4b934b
sooyeon <tndus77@sookmyung.ac.kr>
2021-11-10 18:40:25 +0900

    [ADD] companyReservation presenter, contract

---
+++ app/src/main/java/com/example/myapplication/presentation/company/CompanyReservationContract.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.presentation.company
+
+import com.example.myapplication.data.api.ReservationInfo
+import com.example.myapplication.domain.CompanyInformation
+import com.example.myapplication.presentation.BasePresenter
+import com.example.myapplication.presentation.BaseView
+
+interface CompanyReservationContract {
+    interface View : BaseView<Presenter> {
+        fun processReservationSuccess()
+
+        fun showLoadingIndicator()
+
+        fun hideLoadingIndicator()
+    }
+
+    interface Presenter : BasePresenter{
+        fun makeReservation(companyId: Int, reservationInfo: ReservationInfo)
+    }
+}
--- app/src/main/java/com/example/myapplication/presentation/company/CompanyReservationFragment.kt
+++ app/src/main/java/com/example/myapplication/presentation/company/CompanyReservationFragment.kt
@@ -4,26 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowId
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentCompanyReservationBinding
+import org.koin.android.ext.android.inject
+import org.koin.android.scope.ScopeFragment
 
-class CompanyReservationFragment: Fragment() {
+class CompanyReservationFragment: ScopeFragment(), CompanyReservationContract.View {
     private var binding: FragmentCompanyReservationBinding? = null
+
+    override val presenter: CompanyReservationContract.Presenter by inject()
+
+    private lateinit var bugName: String
+    private lateinit var firstFoundDate: String
+    private lateinit var firstFoundPlace: String
+
+    /*private val hasBugBeenShown by lazy {
+        binding?.
+    }*/
+
+    private lateinit var wantedStartDate: String
+    private lateinit var wantedEndDate: String
+    private lateinit var reserveDateTime: String
+    private lateinit var extraMessage: String
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = FragmentCompanyReservationBinding.inflate(inflater, container, false)
-        .also { binding = it }
+        .also {
+            binding = it
+        }
         .root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         bindViews()
+
+        presenter.onViewCreated()
     }
 
     override fun onDestroyView() {
@@ -34,5 +58,26 @@ class CompanyReservationFragment: Fragment() {
         binding?.btnSubmit?.setOnClickListener {
             findNavController().navigate(R.id.action_companyReservationFragment_to_companyReservationCheckFragment)
         }
+
+        binding?.apply {
+            btnSubmit.setOnClickListener {
+                bugName = etBug.text.toString()
+                firstFoundDate = etFindDate.text.toString()
+                firstFoundPlace = etFindSpot.text.toString()
+
+            }
+        }
+    }
+
+    override fun processReservationSuccess() {
+        view?.findNavController()?.navigate(R.id.action_companyReservationCheckFragment_to_companyReservationCompletedFragment)
+    }
+
+    override fun showLoadingIndicator() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideLoadingIndicator() {
+        TODO("Not yet implemented")
     }
 }
